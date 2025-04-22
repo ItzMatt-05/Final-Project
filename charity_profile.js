a90151566fcae0703eb8d50d930e3a4b884e7466
ItzMatt-05 <mhpineda05@gmail.com>
2025-04-21 20:18:06 -0400

    Create charity_profile.js

---
+++ charity_profile.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("volunteerBtn");
+  const form = document.getElementById("volunteerForm");
+
+  button.addEventListener("click", () => {
+    form.style.display = "block";
+    button.style.display = "none";
+  });
+});
+
+const image = document.getElementById("charityImg");
+const extraInfo = document.getElementById("extraInfo");
+
+image.addEventListener("mouseenter", () => {
+  extaInfo.style.display = "block";
+});
+
+image.addEventListener("mouseleave", () => {
+  extraInfo.style.display = "none";
+});
+                       
